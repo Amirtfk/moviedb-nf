8a898f5e8f692e305eef3936bb95da03665d40ce
Amir Tafkhimi <amir.tafkhimi@gmail.com>
2022-10-07 11:28:06 +0200

    Add deploytoheroku.yml

--- backend/src/main/java/de/neuefische/backend/respository/MovieRepo.java
+++ backend/src/main/java/de/neuefische/backend/respository/MovieRepo.java
@@ -2,21 +2,11 @@ package de.neuefische.backend.respository;
 
 import de.neuefische.backend.model.Movie;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.*;
 
 @Repository
-public class MovieRepo {
+public interface MovieRepo extends MongoRepository <Movie, String> {
 
-    HashMap<String, Movie> movieDB = new HashMap<>();
-
-
-    public List<Movie> findAll() {
-        return new ArrayList<>(movieDB.values());
-    }
-
-    public Movie add(Movie movie) {
-        movieDB.put(movie.getId(), movie);
-        return movie;
-    }
 }
